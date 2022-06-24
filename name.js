90d5571dd5b5a91150ea6c6418844dd92cb2dd12
demiladeogo <demiladeogobankole @gmail.com>
2022-06-24 01:36:40 +0100

    first commit

---
+++ name.js
@@ -0,0 +1,14 @@
+const theName = "demilade Bankole";
+let firstLetter = theName[0].toUpperCase();
+let others = theName.slice(1);
+let result = firstLetter + others;
+console.log(result);
+
+let height = "5.3ft";
+console.log(height);
+
+let country = "nigeria";
+let firstCount = country[0].toUpperCase();
+let countOthers = country.slice(1);
+let myCountResult = firstCount + countOthers;
+console.log(myCountResult);
